499f924bf505eb732f268f4735dd2f53d83978be
martinristow <martinristov084@gmail.com>
2025-05-17 21:10:30 +0200

    Initial Commit

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,6 +1,6 @@
 import {Button} from "@/components/ui/button";
 export default function Home() {
   return (
-      <div><Button>Click me</Button></div>
+      <div><p>This is an authenticated route</p></div>
   )
 }
