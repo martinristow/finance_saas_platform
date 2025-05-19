1c2fc3d3226cb9b9ad6f0700fcb428427b70234d
martinristow <martinristov084@gmail.com>
2025-05-19 23:46:38 +0200

    dummy code

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -5,5 +5,11 @@ export const runtime = 'edge'
 
 const app = new Hono().basePath('/api')
 
+app.get("/hello", (c) => {
+    return c.json({
+        hello: "world"
+    });
+})
+
 export const GET = handle(app)
 export const POST = handle(app)
