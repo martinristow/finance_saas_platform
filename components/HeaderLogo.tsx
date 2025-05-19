0f80b41b19f6b87561ae3e7a13d93b2e747baf75
martinristow <martinristov084@gmail.com>
2025-05-19 23:47:26 +0200

    implement header logo

---
+++ components/HeaderLogo.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
+const HeaderLogo = () => {
+    return (
+        <Link href="/">
+            <div className="items-center hidden lg:flex">
+                <Image src="/logoipsum-361.svg" alt="logo" width={28} height={28}/>
+                <p className="font-semibold text-white text-2xl ml-2.5">
+                    Finance
+                </p>
+            </div>
+        </Link>
+    )
+}
+export default HeaderLogo
