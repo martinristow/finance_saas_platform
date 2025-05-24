c06e5e56406345153dabf42ed02e413b3412d67a
martinristow <martinristov084@gmail.com>
2025-05-24 23:35:52 +0200

    wrapped all app with query provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import {
     ClerkProvider,
 } from '@clerk/nextjs'
+import {QueryProvider} from "@/providers/query-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,7 +32,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+      <QueryProvider>
         {children}
+      </QueryProvider>
       </body>
     </html>
       </ClerkProvider>
