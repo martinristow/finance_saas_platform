226802ec68b33dde619fb7dcc6583fe2f07397a7
martinristow <martinristov084@gmail.com>
2025-05-24 23:50:36 +0200

    displayed account data

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,18 @@
-import {UserButton} from "@clerk/nextjs";
+"use client"
+
+import {useGetAccounts} from "@/features/accounts/api/use-get-accounts";
 
 export default function Home() {
+    const {data: accounts, isLoading} = useGetAccounts();
+
+    if(isLoading) {
+        return <div>Loading...</div>;
+    }
   return (
       <div>
-        Dashboard Page
+          {accounts?.map((account) => (
+              <div key={account.id}>{account.name}</div>
+          ))}
       </div>
   )
 }
