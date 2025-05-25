a73b6a1f686822e7a0c78b19fd7881cc68baa491
martinristow <martinristov084@gmail.com>
2025-05-25 12:32:53 +0200

    implement post method

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,7 +1,9 @@
 import {Hono} from "hono";
 import {db} from "@/database/drizzle"
-import {accounts} from "@/database/schema";
+import {accounts, insertAccountSchema} from "@/database/schema";
 import {clerkMiddleware, getAuth} from "@hono/clerk-auth";
+import { zValidator } from '@hono/zod-validator'
+import {createId} from "@paralleldrive/cuid2";
 
 const app = new Hono()
     .get("/",
@@ -18,6 +20,27 @@ const app = new Hono()
                 name: accounts.name,
             }).from(accounts);
             return c.json({accounts: {data}})
+        })
+    .post("/",
+        clerkMiddleware(),
+        zValidator("json", insertAccountSchema.pick({
+            name: true,
+        })),
+        async (c) => {
+            const auth = getAuth(c);
+            const values = c.req.valid("json")
+
+            if (!auth?.userId) {
+                return c.json({error: "Unauthorized"}, 401);
+            }
+
+            const data = await db.insert(accounts).values({
+                id: createId(),
+                userId: auth.userId,
+                ...values,
+            }).returning();
+
+            return c.json({});
         })
 
 export default app;
