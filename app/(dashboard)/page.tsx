65fc76613ba6b19a5a6c814841558a0d767a9634
martinristow <martinristov084@gmail.com>
2025-05-29 22:16:13 +0200

    implemented button for opening the sheet component

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,18 +1,17 @@
 "use client"
 
-import {useGetAccounts} from "@/features/accounts/api/use-get-accounts";
+import {useNewAccount} from "@/features/accounts/hooks/use-new-account";
+import {Button} from "@/components/ui/button";
 
 export default function Home() {
-    const {data: accounts, isLoading} = useGetAccounts();
 
-    if(isLoading) {
-        return <div>Loading...</div>;
-    }
-  return (
-      <div>
-          {accounts?.map((account) => (
-              <div key={account.id}>{account.name}</div>
-          ))}
-      </div>
-  )
+    const {onOpen} = useNewAccount();
+
+    return (
+        <div>
+            <Button onClick={onOpen}>
+                Add an account
+            </Button>
+        </div>
+    )
 }
