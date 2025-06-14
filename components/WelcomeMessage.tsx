91b806234fb97233cebfa220123abd0d766c5aa4
martinristow <martinristov084@gmail.com>
2025-06-14 19:13:42 +0200

    removed console.log

--- components/WelcomeMessage.tsx
+++ components/WelcomeMessage.tsx
@@ -4,7 +4,6 @@ import {useUser} from "@clerk/nextjs";
 
 const WelcomeMessage = () => {
     const {user, isLoaded} = useUser();
-    console.log(user);
     return (
         <div className="space-y-2 mb-4">
             <h2 className="text-2xl lg:text-4xl text-white font-medium">
